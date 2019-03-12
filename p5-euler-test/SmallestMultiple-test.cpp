1ca18b2aeb0129baf9832c4b24fea000db95a98c
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-03-12 18:01:56 +0100

    Added class SmallestMultiple and file to do gtests for the class

---
+++ p5-euler-test/SmallestMultiple-test.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+#include "../p5-euler/SmallestMultiple.h"
+
+
+class SmallestMultipleTestClass : public testing::Test
+{
+public:
+	SmallestMultiple smallestmultiple;
+};
+
+TEST(SmallestMultipleExists, Exists) {
+	SmallestMultiple smallestmultiple;
+}
---
+++ p5-euler/p5-euler.cpp
@@ -0,0 +1,16 @@
+// p5-euler.cpp
+//
+
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned int p5_answ = 0;
+
+
+	std::cout << "The answer to the fifth problem of ProjectEuler.Net is " << p5_answ << ".\n";
+
+	return 0;
+}
